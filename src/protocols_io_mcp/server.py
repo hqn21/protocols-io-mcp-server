5c945a619975176596b49eaa657b06618e642c66
Hao-Quan Liu <haoquan291@gmail.com>
2026-01-14 08:19:25 +0800

    fix: skip OAuthProxy initialization for stdio transport

--- src/protocols_io_mcp/server.py
+++ src/protocols_io_mcp/server.py
@@ -3,26 +3,34 @@ import importlib
 from fastmcp import FastMCP
 from fastmcp.server.auth import OAuthProxy
 from fastmcp.server.auth.providers.debug import DebugTokenVerifier
+from protocols_io_mcp.utils import config
 from dotenv import load_dotenv
 load_dotenv()
 
-auth = OAuthProxy(
-    upstream_authorization_endpoint="https://www.protocols.io/api/v3/oauth/authorize",
-    upstream_token_endpoint="https://www.protocols.io/api/v3/oauth/token",
-    upstream_client_id=os.getenv("PROTOCOLS_IO_CLIENT_ID"),
-    upstream_client_secret=os.getenv("PROTOCOLS_IO_CLIENT_SECRET"),
-    token_verifier=DebugTokenVerifier(),
-    base_url=os.getenv("PROTOCOLS_IO_MCP_BASE_URL"),
-    valid_scopes=["readwrite"],
-    forward_pkce=False,
-    token_endpoint_auth_method="client_secret_post"
-)
-
-mcp = FastMCP(
-    name="protocols-io-mcp",
-    instructions="""
-        This server helps you interact with data from protocols.io.
-    """,
-    auth=auth
-)
+if config.TRANSPORT_TYPE == "stdio":
+    mcp = FastMCP(
+        name="protocols-io-mcp",
+        instructions="""
+            This server helps you interact with data from protocols.io.
+        """
+    )
+else:
+    auth = OAuthProxy(
+        upstream_authorization_endpoint="https://www.protocols.io/api/v3/oauth/authorize",
+        upstream_token_endpoint="https://www.protocols.io/api/v3/oauth/token",
+        upstream_client_id=os.getenv("PROTOCOLS_IO_CLIENT_ID"),
+        upstream_client_secret=os.getenv("PROTOCOLS_IO_CLIENT_SECRET"),
+        token_verifier=DebugTokenVerifier(),
+        base_url=os.getenv("PROTOCOLS_IO_MCP_BASE_URL"),
+        valid_scopes=["readwrite"],
+        forward_pkce=False,
+        token_endpoint_auth_method="client_secret_post"
+    )
+    mcp = FastMCP(
+        name="protocols-io-mcp",
+        instructions="""
+            This server helps you interact with data from protocols.io.
+        """,
+        auth=auth
+    )
 importlib.import_module('protocols_io_mcp.tools')
